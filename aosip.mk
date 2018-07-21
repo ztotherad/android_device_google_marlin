522c376cfd8aa97ceef8cb9652e4f6feca4f5a38
Steven <stevn4127@gmail.com>
2018-07-21 01:29:20 -0400

    Update and rename lineage.mk to aosip.mk

--- aosip.mk
+++ aosip.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Aosip stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_marlin.mk)
@@ -11,7 +11,7 @@ $(call inherit-product, device/google/marlin/aosp_marlin.mk)
 -include device/google/marlin/marlin/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_marlin
+PRODUCT_NAME := aosip_marlin
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 TARGET_MANUFACTURER := HTC
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM4.171019.021.D1 4768809 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM4.171019.021.P1 4820305 release-keys"
 
-BUILD_FINGERPRINT := google/marlin/marlin:8.1.0/OPM4.171019.021.D1/4768809:user/release-keys
+BUILD_FINGERPRINT := google/marlin/marlin:8.1.0/OPM4.171019.021.P1/4820305:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
