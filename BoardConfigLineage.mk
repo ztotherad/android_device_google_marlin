021be7225947349229904e9416636591f584b68c
Steven <stevn4127@gmail.com>
2018-07-21 01:59:36 -0400

    rename defconfig

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,7 +3,7 @@
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
+TARGET_KERNEL_CONFIG := flash_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 7.0.2
